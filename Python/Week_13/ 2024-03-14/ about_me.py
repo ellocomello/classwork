652c556daf2dcde3097a69c12e900b989a9bde36
ismael <ilara13@bostonk12.org>
2024-03-15 08:12:56 -0400

    about me 2

---
+++ Python/Week_13/ 2024-03-14/ about_me.py
@@ -0,0 +1,7 @@
+# MY name is ismael lara
+#3/14/24
+
+print("i from bani i was born on september 24 in 2009")
+print("i was born in dominican republic")
+print("14")
+print(" my favorite class is physics")
