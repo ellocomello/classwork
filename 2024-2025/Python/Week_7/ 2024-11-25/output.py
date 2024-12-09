c28e3e4772c112ab56ea72a1dab34ab72454f6b4
Ismael Lara <ilara13@bostonk12.org>
2024-12-09 09:32:50 -0500

    klk nuevo clase 12/9

---
+++ 2024-2025/Python/Week_7/ 2024-11-25/output.py
@@ -0,0 +1,10 @@
+#ismael
+#11/26/24
+
+print('ismael')
+print('mello')
+print('lara')
+print('ismael', 'mello','lara')
+print('ismael','mello',"lara",end=".\n")
+print('ismael','mello',"lara",sep="*", end="#\n")
+print('ismael','mello',"lara",sep="#", end=".\n")
