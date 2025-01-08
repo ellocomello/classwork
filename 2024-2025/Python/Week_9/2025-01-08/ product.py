b2b48e87d0c505b23cccee4bed9f4a9ef4670e5a
Ismael Lara <ilara13@bostonk12.org>
2025-01-08 10:08:51 -0500

    klk nuevo clase 1/8

---
+++ 2024-2025/Python/Week_9/2025-01-08/ product.py
@@ -0,0 +1,10 @@
+# ismael
+1/8/2025
+
+num1=int(input("Please enter a number"))
+num2=int(input("Please enter another number"))
+num3=int(input("Please enter a third number"))
+
+product = num1*num2*num3
+print()
+print(num1,"x",num2,"x",num3,"=",product)
