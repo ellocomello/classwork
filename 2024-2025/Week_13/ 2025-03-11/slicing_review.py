e380436c443dc7858e023bfad6dbcfcb8f78db78
Ismael Lara <ilara13@bostonk12.org>
2025-03-12 09:39:32 -0400

    klk nuevo clase 3/11

---
+++ 2024-2025/Week_13/ 2025-03-11/slicing_review.py
@@ -0,0 +1,8 @@
+#ismael
+#3-11-25
+
+first_name=input("what is your first name?:")
+last_name=input("What is last name?")
+print("")
+print("look what I did to your name!")
+print(first_name[::-1], last_name[::-1])
