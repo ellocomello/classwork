bacb189273c4b200ecea8431875162b65d0e6346
Ismael Lara <ilara13@bostonk12.org>
2025-06-13 14:34:30 -0400

    final project math

---
+++ finallllll/final.py
@@ -0,0 +1,8 @@
+#ismael
+# 6/13/2025
+#do the Radius
+
+radius=int(input("Enter the radius of cylinder: "))
+height=int(input("Enter the height of the cylinder: "))
+V=3.14*radius*radius*height
+print(f"A cylinder with a radius of {radius} and a height of {height} will have a volume of {V}")
