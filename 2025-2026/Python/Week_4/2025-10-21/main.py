7531c7cb7f5e2ad6ffa19df409d075a110212214
430133 <ilara13@bostonk12.org>
2025-10-21 11:44:14 -0400

    2025-10-21

---
+++ 2025-2026/Python/Week_4/2025-10-21/main.py
@@ -0,0 +1,22 @@
+def main():
+    name= "ismael"
+    print(name)
+    print(len(name))
+    print(type(name))
+    age = 16
+    print(age)
+    print(len(str(age)))
+    print(type(age))
+    float_age = float(age)
+    print(type(float_age))
+    int_age = int(age)
+    print(type(int_age))
+    planning_on_graduating_on_time = True
+    print(type(planning_on_graduating_on_time))
+    print(planning_on_graduating_on_time)
+    int_planning = int(planning_on_graduating_on_time)
+    print(int_planning)
+    print(type(int_planning))
+    print(f"my name is {name} , i am {age} years old, and my plan is to graduate on time{planning_on_graduating_on_time}")
+if __name__ == '__main__':
+    main()
